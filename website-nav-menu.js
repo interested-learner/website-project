4c3f93055f6151acd13b2dac712a9b41f40162a7
interested-learner <ntcressler@gmail.com>
2026-04-20 10:19:52 -0400

    vercel fix 2

--- website-nav-menu.js
+++ website-nav-menu.js
@@ -23,7 +23,7 @@ export class WebsiteNavMenu extends DDDSuper(LitElement) {
 
   connectedCallback() {
     super.connectedCallback();
-    fetch(new URL("../api/menu.json", import.meta.url).href)
+    fetch("/api/menu.json")
       .then(res => res.json())
       .then(data => {
         this.items = data.items;
